f5cbc8fe30a76fff892d69048553988c8f21030d
Timur <timur.salakhetdinov@gmail.com>
2017-12-18 23:31:01 +0300

    add sample for replace method with method object refactoring

---
+++ src/bloaters/long_method/ReplaceMethodWithMethodObject.java
@@ -0,0 +1,13 @@
+package bloaters.long_method;
+
+class Order {
+    //...
+    public double price() {
+        double primaryBasePrice;
+        double secondaryBasePrice;
+        double tertiaryBasePrice;
+        // long computation.
+        //...
+        return 0;
+    }
+}
