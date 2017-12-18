ff30f19fbad621ea9058360bd36a7db3df654157
Timur <timur.salakhetdinov@gmail.com>
2017-12-18 23:36:10 +0300

    use replace method with method object refactoring

--- src/bloaters/long_method/ReplaceMethodWithMethodObject.java
+++ src/bloaters/long_method/ReplaceMethodWithMethodObject.java
@@ -1,11 +1,28 @@
 package bloaters.long_method;
 
 class Order {
+    public double primaryBasePrice;
+    public double secondaryBasePrice;
+    public double tertiaryBasePrice;
+
     //...
     public double price() {
-        double primaryBasePrice;
-        double secondaryBasePrice;
-        double tertiaryBasePrice;
+        return new PriceCalculator(this).compute();
+    }
+}
+
+class PriceCalculator {
+    private double primaryBasePrice;
+    private double secondaryBasePrice;
+    private double tertiaryBasePrice;
+
+    public PriceCalculator(Order order) {
+        primaryBasePrice = order.primaryBasePrice;
+        secondaryBasePrice = order.secondaryBasePrice;
+        tertiaryBasePrice = order.tertiaryBasePrice;
+    }
+
+    public double compute() {
         // long computation.
         //...
         return 0;
