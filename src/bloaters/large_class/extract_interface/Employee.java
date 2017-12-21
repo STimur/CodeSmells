b4afd63f60fed291ac82802796d26f19c6319680
Timur <timur.salakhetdinov@gmail.com>
2017-12-21 22:09:26 +0300

    add sample for extract interface refactoring

---
+++ src/bloaters/large_class/extract_interface/Employee.java
@@ -0,0 +1,19 @@
+package bloaters.large_class.extract_interface;
+
+public class Employee {
+    public int getRate() {
+        return 0;
+    }
+
+    public boolean hasSpecialSkill() {
+        return false;
+    }
+
+    public String getName() {
+        return null;
+    }
+
+    public String getDepartment() {
+        return null;
+    }
+}
