8acb8da48b86fe1897daa4752824970335f74fae
Timur <timur.salakhetdinov@gmail.com>
2017-12-21 22:21:06 +0300

    add sample for duplicate observed data refactoring

---
+++ src/bloaters/large_class/duplicate_observed_data/IntervalWindow.java
@@ -0,0 +1,24 @@
+package bloaters.large_class.duplicate_observed_data;
+
+import javafx.scene.control.TextField;
+
+public class IntervalWindow {
+    TextField startField;
+    TextField endField;
+    TextField lengthField;
+
+    public void StartField_FocusLost() {
+    }
+
+    public void EndField_FocusLost() {
+    }
+
+    public void LengthField_FocusLost() {
+    }
+
+    public void calculateLength() {
+    }
+
+    public void calculateEnd() {
+    }
+}
