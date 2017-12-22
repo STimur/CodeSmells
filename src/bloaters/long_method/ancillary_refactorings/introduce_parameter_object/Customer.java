ebfe648cc43be79bbac3e54ffeb7b4ddba64f661
Timur <timur.salakhetdinov@gmail.com>
2017-12-22 05:24:43 +0300

    apply introduce parameter object refactoring to prepare for disposing of long method smell

--- src/bloaters/long_method/ancillary_refactorings/introduce_parameter_object/Customer.java
+++ src/bloaters/long_method/ancillary_refactorings/introduce_parameter_object/Customer.java
@@ -1,17 +1,15 @@
 package bloaters.long_method.ancillary_refactorings.introduce_parameter_object;
 
-import java.util.Date;
-
 public class Customer {
-    public int amountInvoicedIn(Date start, Date end) {
+    public int amountInvoicedIn(DateRange dateRange) {
         return 0;
     }
 
-    public int amountReceivedIn(Date start, Date end) {
+    public int amountReceivedIn(DateRange dateRange) {
         return 0;
     }
 
-    public int amountOverdueIn(Date start, Date end) {
+    public int amountOverdueIn(DateRange dateRange) {
         return 0;
     }
 }
