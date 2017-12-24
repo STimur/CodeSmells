045814db689ca21333b74ef2088abca740d04b7d
Timur <timur.salakhetdinov@gmail.com>
2017-12-24 03:57:51 +0300

    add sample for replace array with object refactoring

---
+++ src/bloaters/primitive_obsession/replace_array_with_object/FootballClub.java
@@ -0,0 +1,11 @@
+package bloaters.primitive_obsession.replace_array_with_object;
+
+public class FootballClub {
+    String[] row = new String[3];
+
+    public void setSeasonStatistics() {
+        row[0] = "10";
+        row[1] = "15";
+        row[2] = "4";
+    }
+}
