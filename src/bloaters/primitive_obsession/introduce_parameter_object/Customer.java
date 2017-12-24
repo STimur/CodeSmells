2556b86e83af425d7d4e363f636f58f91c65d58c
Timur <timur.salakhetdinov@gmail.com>
2017-12-24 04:04:50 +0300

    copy introduce parameter object and preserve whole object refactorings as they have been already examined before

---
+++ src/bloaters/primitive_obsession/introduce_parameter_object/Customer.java
@@ -0,0 +1,15 @@
+package bloaters.primitive_obsession.introduce_parameter_object;
+
+public class Customer {
+    public int amountInvoicedIn(DateRange dateRange) {
+        return 0;
+    }
+
+    public int amountReceivedIn(DateRange dateRange) {
+        return 0;
+    }
+
+    public int amountOverdueIn(DateRange dateRange) {
+        return 0;
+    }
+}
---
+++ src/bloaters/primitive_obsession/preserve_whole_object/Sample.java
@@ -0,0 +1,28 @@
+package bloaters.primitive_obsession.preserve_whole_object;
+
+public class Sample {
+    private MyRange daysTempRange;
+    private MyPlan plan;
+
+    boolean isWithinPlan() {
+        return plan.withinRange(daysTempRange);
+    }
+
+    private class MyRange {
+        public int getLow() {
+            return 0;
+        }
+
+        public int getHigh() {
+            return 0;
+        }
+    }
+
+    private class MyPlan {
+        final int value = 10;
+
+        public boolean withinRange(MyRange range) {
+            return (range.getLow() < value && value < range.getHigh());
+        }
+    }
+}
