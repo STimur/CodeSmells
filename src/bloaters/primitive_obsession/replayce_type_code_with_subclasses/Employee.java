eb089d3d8bba0e1312b17912519dab3d65b4414e
Timur <timur.salakhetdinov@gmail.com>
2017-12-24 03:15:07 +0300

    use replace type code with subclasses refactoring to dispose of primitive obsession smell

--- src/bloaters/primitive_obsession/replayce_type_code_with_subclasses/Employee.java
+++ src/bloaters/primitive_obsession/replayce_type_code_with_subclasses/Employee.java
@@ -1,15 +1,5 @@
 package bloaters.primitive_obsession.replayce_type_code_with_subclasses;
 
-public class Employee {
-    final int ENGINEER = 0;
-    final int SALESMAN = 1;
-    int type;
-
-    public String getProfession() {
-        if (type == ENGINEER)
-            return "Engineer";
-        if (type == SALESMAN)
-            return "SALESMAN";
-        return "UNDEFINED";
-    }
+public abstract class Employee {
+    public abstract String getProfession();
 }
---
+++ src/bloaters/primitive_obsession/replayce_type_code_with_subclasses/Engineer.java
@@ -0,0 +1,8 @@
+package bloaters.primitive_obsession.replayce_type_code_with_subclasses;
+
+public class Engineer extends Employee {
+    @Override
+    public String getProfession() {
+        return "Engineer";
+    }
+}
