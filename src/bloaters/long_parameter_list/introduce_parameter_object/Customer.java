5b99ae18c2f593f0f300b645f106e4c0a97fd6ad
Timur <timur.salakhetdinov@gmail.com>
2017-12-24 23:26:19 +0300

    copy introduce parameter object and preserve whole object refactorings as methods to dispose of long parameter list smell as they have been already examined before

---
+++ src/bloaters/long_parameter_list/introduce_parameter_object/Customer.java
@@ -0,0 +1,15 @@
+package bloaters.long_parameter_list.introduce_parameter_object;
+
+public class Customer {
+    public int amountInvoicedIn(DateRange dateRange) {
+        return 0;
+    }
+
+    public int amountReceivedIn(DateRange dateRange) {
+        return 0;
+    }
+
+    public int amountOverdueIn(DateRange dateRange) {
+        return 0;
+    }
+}
