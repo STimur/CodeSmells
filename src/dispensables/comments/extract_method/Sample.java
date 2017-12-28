2ae9c372310653b4baf4a44e5684e876dacc6ec9
Timur <timur.salakhetdinov@gmail.com>
2017-12-28 07:18:47 +0300

    copy extract method refactoring as method to dispose of comment smell

---
+++ src/dispensables/comments/extract_method/Sample.java
@@ -0,0 +1,22 @@
+package dispensables.comments.extract_method;
+
+public class Sample {
+    private String name;
+
+    void printOwing() {
+        printBanner();
+        printDetails();
+    }
+
+    private void printDetails() {
+        System.out.println("name: " + name);
+        System.out.println("amount: " + getOutstanding());
+    }
+
+    private String getOutstanding() {
+        return null;
+    }
+
+    private void printBanner() {
+    }
+}
