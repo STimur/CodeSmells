22e77ca7e24ff8db7b1ee69115da15195bd56d76
Timur <timur.salakhetdinov@gmail.com>
2017-12-28 10:45:34 +0300

    add sample for consolidate duplicate conditional fragments refactoring to dispose of duplicate code smell

---
+++ src/dispensables/duplicate_code/consolidate_duplicate_conditional_fragments/Product.java
@@ -0,0 +1,23 @@
+package dispensables.duplicate_code.consolidate_duplicate_conditional_fragments;
+
+public class Product {
+    private double total;
+    private double price;
+
+    void market() {
+        if (isSpecialDeal()) {
+            total = price * 0.95;
+            send();
+        } else {
+            total = price * 0.98;
+            send();
+        }
+    }
+
+    private void send() {
+    }
+
+    private boolean isSpecialDeal() {
+        return false;
+    }
+}
