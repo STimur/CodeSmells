771614b74f88f55a5f2c09ab4a92df7d1680428a
Timur <timur.salakhetdinov@gmail.com>
2017-12-28 10:08:47 +0300

    add sample for extract superclass refactoring to dispose of duplicate code smell

---
+++ src/dispensables/duplicate_code/extract_superclass/Department.java
@@ -0,0 +1,18 @@
+package dispensables.duplicate_code.extract_superclass;
+
+public class Department {
+    double getTotalAnnualCost() {
+        //...
+        return 0;
+    }
+
+    String getName() {
+        //...
+        return null;
+    }
+
+    int getHeadCount() {
+        //...
+        return 0;
+    }
+}
---
+++ src/dispensables/duplicate_code/extract_superclass/Employee.java
@@ -0,0 +1,18 @@
+package dispensables.duplicate_code.extract_superclass;
+
+public class Employee {
+    double getAnnualCost() {
+        //...
+        return 0;
+    }
+
+    String getName() {
+        //...
+        return null;
+    }
+
+    int getId() {
+        //...
+        return 0;
+    }
+}
