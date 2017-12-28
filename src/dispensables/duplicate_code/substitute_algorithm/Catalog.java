b1551a7746d6bcbd6e2b895499396698b33c2cf5
Timur <timur.salakhetdinov@gmail.com>
2017-12-28 10:03:57 +0300

    use substitute algorithm refactoring to dispose of duplicate code smell

--- src/dispensables/duplicate_code/substitute_algorithm/Catalog.java
+++ src/dispensables/duplicate_code/substitute_algorithm/Catalog.java
@@ -5,21 +5,6 @@ import java.util.List;
 
 public class Catalog {
     String foundPerson(String[] people) {
-        for (int i = 0; i < people.length; i++) {
-            if (people[i].equals("Don")) {
-                return "Don";
-            }
-            if (people[i].equals("John")) {
-                return "John";
-            }
-            if (people[i].equals("Kent")) {
-                return "Kent";
-            }
-        }
-        return "";
-    }
-
-    String foundPersonNew(String[] people) {
         List candidates =
                 Arrays.asList(new String[]{"Don", "John", "Kent"});
         for (int i = 0; i < people.length; i++)
