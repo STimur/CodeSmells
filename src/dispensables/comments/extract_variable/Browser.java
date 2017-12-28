f9dc0a639c001b3d27e4b1c2edd3a778b57d9c26
Timur <timur.salakhetdinov@gmail.com>
2017-12-28 07:15:34 +0300

    use extract variable refactoring to dispose of comment smell

--- src/dispensables/comments/extract_variable/Browser.java
+++ src/dispensables/comments/extract_variable/Browser.java
@@ -6,10 +6,11 @@ public class Browser {
     private int resize;
 
     void renderBanner() {
-        // some lengthy explanation what this complex condition means...
-        if ((platform.toUpperCase().indexOf("MAC") > -1) &&
-                (browser.toUpperCase().indexOf("IE") > -1) &&
-                wasInitialized() && resize > 0 )
+        final boolean isMacOs = platform.toUpperCase().indexOf("MAC") > -1;;
+        final boolean isIE = browser.toUpperCase().indexOf("IE") > -1;
+        final boolean wasResized = resize > 0;
+
+        if (isMacOs && isIE && wasInitialized() && wasResized)
         {
             // do something
         }
