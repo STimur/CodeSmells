301741362a85a15db24f4c2fb0d046572ecc81d8
Timur <timur.salakhetdinov@gmail.com>
2017-12-28 08:18:59 +0300

    use pull up method refactoring to dispose of duplicate code smell

--- src/dispensables/duplicate_code/pull_up_method/Shop.java
+++ src/dispensables/duplicate_code/pull_up_method/Shop.java
@@ -1,20 +1,15 @@
 package dispensables.duplicate_code.pull_up_method;
 
 public class Shop {
+    public void printReceipt() {
+        System.out.println("Today's date");
+        System.out.println("Items chosen");
+        System.out.println("Thank you foo coming!");
+    }
 }
 
 class Grocery extends Shop {
-    public void printReceipt() {
-        System.out.println("Today's date");
-        System.out.println("Items chosen");
-        System.out.println("Thank you foo coming!");
-    }
 }
 
 class BookStore extends Shop {
-    public void printReceipt() {
-        System.out.println("Today's date");
-        System.out.println("Items chosen");
-        System.out.println("Thank you foo coming!");
-    }
 }
