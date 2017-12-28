96dfdd52241571e8a2c91c67b4325ee6c2df0ca8
Timur <timur.salakhetdinov@gmail.com>
2017-12-28 09:45:01 +0300

    use form template method refactoring to dispose of duplicate code smell

--- src/dispensables/duplicate_code/form_template_method/ItalianRestaurant.java
+++ src/dispensables/duplicate_code/form_template_method/ItalianRestaurant.java
@@ -1,23 +1,54 @@
 package dispensables.duplicate_code.form_template_method;
 
-class ItalianRestaurant {
+abstract class Restaurant {
     public void serveDinner() {
+        serveCoffee();
+        serveSoup();
+        serveSalad();
+        serveMainDish();
+        serveDrink();
+    }
+
+    private void serveCoffee() {
         System.out.println("Coffee");
-        System.out.println("Tomato soup");
+    }
+
+    protected abstract void serveSoup();
+
+    private void serveSalad() {
         System.out.println("Salad");
-        System.out.println("Pasta");
+    }
+
+    protected abstract void serveMainDish();
+
+    private void serveDrink() {
         System.out.println("Juice");
     }
 }
 
-class FrenchRestaurant {
-    public void serveDinner() {
-        System.out.println("Coffee");
-        System.out.println("Onion soup");
-        System.out.println("Salad");
-        System.out.println("French chicken");
-        System.out.println("Juice");
+class ItalianRestaurant extends Restaurant {
+    @Override
+    protected void serveSoup() {
+        System.out.println("Tomato soup");
     }
+
+    @Override
+    protected void serveMainDish() {
+        System.out.println("Pasta");
+    }
+}
+
+class FrenchRestaurant extends Restaurant {
+    @Override
+    protected void serveSoup() {
+        System.out.println("Onion soup");
+    }
+
+    @Override
+    protected void serveMainDish() {
+        System.out.println("French chicken");
+    }
+
 }
 
 
