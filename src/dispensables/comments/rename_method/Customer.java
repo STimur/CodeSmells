281f00771c2a2648e9e356073d5541a3171706e9
Timur <timur.salakhetdinov@gmail.com>
2017-12-28 07:22:08 +0300

    add sample for rename method refactoring

---
+++ src/dispensables/comments/rename_method/Customer.java
@@ -0,0 +1,11 @@
+package dispensables.comments.rename_method;
+
+public class Customer {
+    private String name;
+    private String surname;
+
+    // use this method to get full name of the customer
+    public String zuperFun() {
+        return name + surname;
+    }
+}
