986d176a47dd5bdc7b3f4b5f9d22eda73bc52375
Timur <timur.salakhetdinov@gmail.com>
2017-12-28 09:32:49 +0300

    add sample for form template method refactoring to dispose of duplicate code smell

---
+++ src/dispensables/duplicate_code/form_template_method/ItalianRestaurant.java
@@ -0,0 +1,25 @@
+package dispensables.duplicate_code.form_template_method;
+
+class ItalianRestaurant {
+    public void serveDinner() {
+        System.out.println("Coffee");
+        System.out.println("Tomato soup");
+        System.out.println("Salad");
+        System.out.println("Pasta");
+        System.out.println("Juice");
+    }
+}
+
+class FrenchRestaurant {
+    public void serveDinner() {
+        System.out.println("Coffee");
+        System.out.println("Onion soup");
+        System.out.println("Salad");
+        System.out.println("French chicken");
+        System.out.println("Juice");
+    }
+}
+
+
+
+
