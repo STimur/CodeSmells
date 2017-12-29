23f1668cf5555dd402f8e955520f30799d3a144c
Timur <timur.salakhetdinov@gmail.com>
2017-12-29 07:11:31 +0300

    use remove middle man refactoring to dispose of middle man smell

--- src/couplers/middle_man/remove_middle_man/Person.java
+++ src/couplers/middle_man/remove_middle_man/Person.java
@@ -1,9 +1,5 @@
 package couplers.middle_man.remove_middle_man;
 
 public class Person {
-    private Department department;
-
-    public Manager getManager() {
-        return department.getManager();
-    }
+    public Department department;
 }
