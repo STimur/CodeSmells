cac2566d86aaea00b37544a8531ffab80392ea32
Timur <timur.salakhetdinov@gmail.com>
2017-12-29 06:42:21 +0300

    add sample for replace delegation with inheritance refactoring to dispose of inappropriate intimacy smell

---
+++ src/couplers/inappropriate_intimacy/replace_delegation_with_inheritance/Employee.java
@@ -0,0 +1,9 @@
+package couplers.inappropriate_intimacy.replace_delegation_with_inheritance;
+
+public class Employee {
+    Person person;
+
+    public String getName() {
+        return person.name;
+    }
+}
---
+++ src/couplers/inappropriate_intimacy/replace_delegation_with_inheritance/Person.java
@@ -0,0 +1,5 @@
+package couplers.inappropriate_intimacy.replace_delegation_with_inheritance;
+
+public class Person {
+    public String name;
+}
