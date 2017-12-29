4704933d8120ae7cf27dbd8c864e3e54d099db59
Timur <timur.salakhetdinov@gmail.com>
2017-12-29 07:28:24 +0300

    add sample for introduce local extension refactoring to dispose of incomplete library class smell

---
+++ src/couplers/incomplete_library_class/introduce_local_extension/Client.java
@@ -0,0 +1,9 @@
+package couplers.incomplete_library_class.introduce_local_extension;
+
+import java.util.Date;
+
+public class Client {
+    public static void main(String[] args) {
+        System.out.println(new Date().nextDay());
+    }
+}
