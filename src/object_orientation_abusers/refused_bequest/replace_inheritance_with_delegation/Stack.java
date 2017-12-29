64a8b44c3e8292120d6aca4263959f7f1e9456eb
Timur <timur.salakhetdinov@gmail.com>
2017-12-29 05:08:08 +0300

    use replace inheritance with delegation refactoring to dispose of refused bequest smell

--- src/object_orientation_abusers/refused_bequest/replace_inheritance_with_delegation/Stack.java
+++ src/object_orientation_abusers/refused_bequest/replace_inheritance_with_delegation/Stack.java
@@ -1,4 +1,9 @@
 package object_orientation_abusers.refused_bequest.replace_inheritance_with_delegation;
 
-public class Stack extends Vector {
+public class Stack {
+    Vector vector;
+
+    public boolean isEmpty() {
+        return vector.isEmpty();
+    }
 }
