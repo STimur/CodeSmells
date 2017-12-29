66aa030be793245437e1856f89a3e042198c9176
Timur <timur.salakhetdinov@gmail.com>
2017-12-29 06:44:41 +0300

    use replace delegation with inheritance refactoring to dispose of inappropriate intimacy smell

--- src/couplers/inappropriate_intimacy/replace_delegation_with_inheritance/Employee.java
+++ src/couplers/inappropriate_intimacy/replace_delegation_with_inheritance/Employee.java
@@ -1,9 +1,9 @@
 package couplers.inappropriate_intimacy.replace_delegation_with_inheritance;
 
-public class Employee {
-    Person person;
-
-    public String getName() {
-        return person.name;
+public class Employee extends Person {
+    public void printReport() {
+        System.out.println("Report for " + name);
+        System.out.println("Some statistics");
+        System.out.println("Summary");
     }
 }
